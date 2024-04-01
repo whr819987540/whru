95255932f7ec0f5ff8303278f20149e190c0c528
whr819987540 <819987540@qq.com>
2024-04-01 21:47:08 +0800

    test log_file

--- tests/test_log.py
+++ tests/test_log.py
@@ -1,5 +1,6 @@
 from whru.log import get_logger
-
+from whru.utils import path_join, get_abs_path, get_datetime_str
 
 if __name__ == "__main__":
-    get_logger("test", "t")
+    logger = get_logger("INFO", "test", path_join(get_abs_path(__file__), "../test_data", f"{get_datetime_str()}.log"))
+    logger.info("hello world")
