84b4e887f06e621733bd507c1c57ba7b73400849
whr819987540 <819987540@qq.com>
2024-04-01 21:34:59 +0800

    0.3 specify packages

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='whru',
-    version='0.2',
+    version='0.3',
     description='My custom Python module',
     author='whr819987540',
     author_email='steeliron550@gmail.com',
-    packages=['whru'],
+    packages=find_packages(),
     install_requires=[
         "torch>=1.7.0",
         "tensorflow>=2.0.2",
